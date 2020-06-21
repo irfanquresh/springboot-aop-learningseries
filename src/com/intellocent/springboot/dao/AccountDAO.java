4bf145553b5ff37f68694f50470c6f2ec6fd9861
Irfan Quresh <irfan.quresh@datamigrationresources.com>
2020-06-21 20:41:06 +0530

    SB AOP INIT...

--- src/com/intellocent/springboot/dao/AccountDAO.java
+++ src/com/intellocent/springboot/dao/AccountDAO.java
@@ -7,6 +7,6 @@ public class AccountDAO {
 
 	public void addAccount() throws Exception {
 		System.out.println(getClass() + ": Doing DB Work...");
-		throw new Exception();
+		//throw new Exception();
 	}
 }
